5e4a7e30a58470ca19ec9275c52950992811d962
Naci Aydogan <noa351@hotmail.de>
2024-12-16 16:38:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className={"p-64"}>GR1</h1>
+    </>
+  );
+}
+
+export default App;
