521469bc2c280b6b8c5377596bcff56fe9e7d2dc
Andre Witt <dev.code@h-town.net>
2024-12-18 01:06:12 +0100

    NEW Component Newentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./index.css";
+import Newentry from "./components/NewEntry/Newentry";
+import Header from "./components/andwit/Header";
+import Footer from "./components/andwit/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className={"p-64"}>GR1</h1>
+      <Header />
+      <Newentry />
+      <Footer />
     </>
   );
 }
