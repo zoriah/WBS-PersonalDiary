fe5eb291e81a34f263f2ca60d785d372aff511f4
Andre Witt <dev.code@h-town.net>
2024-12-19 11:41:14 +0100

    ready for merge

---
+++ src/components/andwit/Footer.jsx
@@ -0,0 +1,21 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="text-center">
+        <div className="up_down">
+          <span>i</span>
+          <span>n</span>
+          <span>s</span>
+          <span>p</span>
+          <span>i</span>
+          <span>r</span>
+          <span>e</span>
+          <span>d</span>
+        </div>
+        by WBS-Coding-School &copy; 2024
+      </footer>
+    </>
+  );
+}
---
+++ src/components/andwit/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1 className="font-sour antialiased text-center text-2xl my-6">
+          Personal Diary
+        </h1>
+      </header>
+    </>
+  );
+}
