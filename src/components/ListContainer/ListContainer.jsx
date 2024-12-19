62f0e9c26d8979284049db148c9c64e56ce296e4
Andre Witt <dev.code@h-town.net>
2024-12-19 10:39:24 +0100

    bugfixin and reorganisation

--- src/components/ListContainer/ListContainer.jsx
+++ src/components/ListContainer/ListContainer.jsx
@@ -5,7 +5,10 @@ import "./ListContainer.css";
 export default function Listcontainer() {
   const arrEntries = entries();
   return (
-    <div>
+    <div id="list-container">
+      <div id="control">
+        <button>Add new Entry</button>
+      </div>
       <div id="entry-list">
         {arrEntries.map((entry, index) => {
           return (
