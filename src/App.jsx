e2aa151f3a6efe350154b60cf275242d388f8188
Naci Aydogan <noa351@hotmail.de>
2024-12-19 11:13:10 +0100

    new update Bigcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Bigcard from "./components/Bigcard";
+// import Smallcards from "./components/Smallcards";
 import "./index.css";
 
 function App() {
   return (
     <>
       <Bigcard />
+      {/* <Smallcards /> */}
     </>
   );
 }
--- src/components/Bigcard.jsx
+++ src/components/Bigcard.jsx
@@ -1,6 +1,63 @@
 import "./bigCard.css";
+
+localStorage.setItem(
+  "entries",
+  JSON.stringify([
+    {
+      title: "card1",
+      entryDate: new Date("February 7, 2010"),
+      actualDate: Date.now(),
+      imdUrl: url("https://picsum.photos/800/600"),
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, dolorem.",
+    },
+    {
+      title: "card2",
+      entryDate: new Date("February 8, 2010"),
+      actualDate: Date.now(),
+      message:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum labore explicabo alias!",
+    },
+    {
+      title: "card3",
+      entryDate: new Date("February 9, 2010"),
+      actualDate: Date.now(),
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem distinctio maiores expedita sapiente iste minima officiis tenetur reprehenderit a ad!",
+    },
+    {
+      title: "card4",
+      entryDate: new Date("February 10, 2010"),
+      actualDate: Date.now(),
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius dolores ducimus quos, voluptate dolor cupiditate?",
+    },
+    {
+      title: "card5",
+      entryDate: new Date("February 11, 2010"),
+      actualDate: Date.now(),
+      message:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, eveniet fugit? Modi beatae ea, ducimus fugit exercitationem nulla inventore incidunt libero qui! Veniam, dolorum rerum.",
+    },
+    {
+      title: "card6",
+      entryDate: new Date("February 12, 2010"),
+      actualDate: Date.now(),
+      message: null,
+    },
+    {
+      title: "card7",
+      entryDate: new Date("February 13, 2010"),
+      actualDate: Date.now(),
+      message: "Hi guys, i like Döner Kebab! Kiss :)",
+    },
+  ])
+);
+
+let entries = JSON.parse(localStorage.getItem("entries"));
+for (let i = 0; i <= 6; i++) console.log(entries[i]);
+
 function Bigcard() {
-  const modalContainer = document.getElementById("modal-container");
   return (
     <div id="modal-container">
       <div className="overflow-hidden" id="modal">
@@ -14,20 +71,7 @@ function Bigcard() {
           </div>
         </div>
         <div id="modal-content">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-            amet quae non! Sapiente obcaecati, maxime saepe tempora quam
-            perspiciatis officia adipisci? Facere modi illo exercitationem
-            ducimus unde itaque labore veniam, ullam quae quam voluptatem ipsum
-            mollitia culpa neque, quibusdam tempore nemo quidem sunt ipsa vel
-            consequatur libero illum. Eaque provident temporibus, vero iusto
-            eius sapiente doloremque animi aperiam labore ipsam fuga minima
-            nesciunt rerum vitae corporis. Itaque voluptatum nemo ex harum neque
-            exercitationem commodi ullam, natus nihil assumenda accusamus
-            mollitia eos? Iusto omnis qui fuga modi odio, recusandae nostrum
-            libero atque error repellat neque eligendi, nobis provident
-            aspernatur doloribus molestiae?
-          </p>
+          <p>{}</p>
         </div>
         <div id="modal-footer">
           {/* <button id="btn-sub">submit</button>
